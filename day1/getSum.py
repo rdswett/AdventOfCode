bf1ba28abb399396c0033472feb88eea71c010b0
Ryan Swett <rswett@fonzinc.com>
2020-12-02 11:35:37 -0700

    Day1 and Day2 solutions

---
+++ day1/getSum.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import os 
+import sys
+import array
+
+inputs = []
+with open('input.txt') as f:
+    for line in f:
+        line = line.split()
+        if line:
+            inputs.append(int(''.join(line)))
+
+print(f"Total Entries: {len(inputs)}")
+
+for i in inputs:
+    #print(f"{i}")
+    for j in inputs:
+        if inputs.index(i) is inputs.index(j):
+            continue
+        total = i+j
+        #print(f"{i} + {j} = {total}")
+        if total == 2020:
+            sum = i*j
+            print(f"{sum}")
+            sys.exit()
